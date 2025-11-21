3854d6068f48f446707a4abfe4a681fa187cea65
Skai <tyronekisienya01@gmail.com>
2025-11-21 11:31:43 +0300

    indexing the product frequent searches, postgressql setup, requirements upgrade for possible docker and render. Debateable

--- products/models.py
+++ products/models.py
@@ -11,12 +11,19 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, db_index=True)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10,  decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    price = models.DecimalField(max_digits=10,  decimal_places=2, db_index=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        indexes = [
+            models.Index(fields=['name']),
+            models.Index(fields=['price']),
+            models.Index(fields=['category']),
+        ]
+        
     def __str__(self):
         return self.name
