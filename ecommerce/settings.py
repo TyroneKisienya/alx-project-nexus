ab2830e79e0a2b0825eac6f467260c494b382aa6
Skai <tyronekisienya01@gmail.com>
2025-11-27 13:16:36 +0300

    render link added

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -33,7 +33,7 @@ DEBUG = config("DEBUG", default=True, cast=bool)
 
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
-    default="localhost,127.0.0.1,https://alx-project-nexus-f8lw.onrender.com"
+    default="localhost,127.0.0.1,alx-project-nexus-f8lw.onrender.com"
 ).split(",")
 
 # SECURITY WARNING: don't run with debug turned on in production!
